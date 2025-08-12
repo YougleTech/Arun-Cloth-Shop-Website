717c55e3f5b22dd78d88a42cda5e55b7cb05a555
Rajababu Kushwaha <kushwahr@roehampton.ac.uk>
2025-08-12 11:14:20 +0100

    Remove @types/swiper and add custom CSS module declarations
    Removed the @types/swiper package from dependencies and added a global.d.ts file to declare modules for Swiper CSS imports. This allows TypeScript to recognize Swiper CSS imports without the need for the @types/swiper package.

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,12 @@
+// Declare modules for CSS imports so TS won't complain
+declare module 'swiper/css' {
+  const content: string;
+  export default content;
+}
+
+declare module 'swiper/css/effect-fade' {
+  const content: string;
+  export default content;
+}
+
+// You can add more if you import other swiper css files
